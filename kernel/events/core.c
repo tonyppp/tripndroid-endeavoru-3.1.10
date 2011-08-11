f5f53dadc1ff905c889325e7388c908bbaa494c3
Mark Rutland <mark.rutland@arm.com>
2011-08-11 12:31:20 +0100

    perf: provide PMU when initing events
    Currently, an event's 'pmu' field is set after pmu::event_init() is
    called. This means that pmu::event_init() must figure out which struct
    pmu the event was initialised from. This makes it difficult to
    consolidate common event initialisation code for similar PMUs, and
    very difficult to implement drivers for PMUs which can have multiple
    instances (e.g. a USB controller PMU, a GPU PMU, etc).
    
    This patch sets the 'pmu' field before initialising the event, allowing
    event init code to identify the struct pmu instance easily. In the
    event of failure to initialise an event, the event is destroyed via
    kfree() without calling perf_event::destroy(), so this shouldn't
    result in bad behaviour even if the destroy field was set before
    failure to initialise was noted.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Reviewed-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link: http://lkml.kernel.org/r/1313062280-19123-1-git-send-email-mark.rutland@arm.com
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -5759,6 +5759,7 @@ struct pmu *perf_init_event(struct perf_event *event)
 	pmu = idr_find(&pmu_idr, event->attr.type);
 	rcu_read_unlock();
 	if (pmu) {
+		event->pmu = pmu;
 		ret = pmu->event_init(event);
 		if (ret)
 			pmu = ERR_PTR(ret);
@@ -5766,6 +5767,7 @@ struct pmu *perf_init_event(struct perf_event *event)
 	}
 
 	list_for_each_entry_rcu(pmu, &pmus, entry) {
+		event->pmu = pmu;
 		ret = pmu->event_init(event);
 		if (!ret)
 			goto unlock;
@@ -5891,8 +5893,6 @@ done:
 		kfree(event);
 		return ERR_PTR(err);
 	}
-
-	event->pmu = pmu;
 
 	if (!event->parent) {
 		if (event->attach_state & PERF_ATTACH_TASK)
