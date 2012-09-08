5a83f9b109f8f5d8e545ea282850f66584ab6fb0
TripNRaVeR <noerialexeew@live.nl>
2012-09-08 19:00:51 +0200

    Revert "patch: fix nct1008 driver when building for endeavoru"
    This reverts commit 7b7c19c0a84c50e9ade7e9466b34dddfcc264176.

--- include/linux/nct1008.h
+++ include/linux/nct1008.h
@@ -47,9 +47,6 @@ struct nct1008_platform_data {
 	u8 thermal_zones_sz;
 	void (*alarm_fn)(bool raised);
 	void (*probe_callback)(struct nct1008_data *);
-#ifdef CONFIG_MACH_ENDEAVORU
-	char *reg_name;
-#endif
 };
 
 struct nct1008_data {
